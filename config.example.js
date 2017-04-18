0d7e26451198c4767a6434fbe57ec6acd449dc7a
mirman-mtaggart <mtaggart@mirman.org>
2017-04-17 22:55:33 -0700

    Example config

---
+++ config.example.js
@@ -0,0 +1,11 @@
+const config = {
+    server: {
+        listenPort: 3000
+    },
+    mongo: {
+        serverUrl: "mongodb://localhost:27017",
+        db: "trakr"
+    }
+}
+
+module.exports = config;
