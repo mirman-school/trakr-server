c0a0329bb8c607b99e1d18856e4d206fe614a156
mirman-mtaggart <mtaggart@mirman.org>
2017-04-26 13:14:37 -0700

    Semicolon

--- models/School.js
+++ models/School.js
@@ -4,6 +4,6 @@ const Schema = mongoose.Schema;
 const schoolSchema = new Schema({
     longName: { type: String, required: true },
     shortName: { type: String, required: true },
-})
+});
 
 module.exports = mongoose.model("School", schoolSchema);
