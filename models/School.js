598df775a94d39d920afbdb8613547a68f2ee752
mirman-mtaggart <mtaggart@mirman.org>
2017-04-26 13:57:09 -0700

    Make schools a subdoc of leagues

--- models/School.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const schoolSchema = new Schema({
-    longName: { type: String, required: true },
-    shortName: { type: String, required: true },
-});
-
-module.exports = mongoose.model("School", schoolSchema);
