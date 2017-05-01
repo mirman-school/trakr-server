082a8b687f195bb6190b7ba737393f6cc1da8ff6
mirman-wauhls2017 <wauhls2017@mirmanmustangs.org>
2017-05-01 13:55:51 -0700

    Worked on athelete/meet

---
+++ models/Athlete.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const athleteSchema = new Schema({
+    name: { type: String, required: true },
+    school: { type: Objectid, required: true }
+})
+
+module.exports = mongoose.model("Athlete", athleteSchema);
