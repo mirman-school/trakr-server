debf07890c320729f3295e731f9b337bd8a364a4
mirman-kcheng <kacheng2017@mirmanmustangs.org>
2017-05-01 14:02:04 -0700

    updated event

--- models/Event.js
+++ models/Event.js
@@ -5,7 +5,11 @@ const eventSchema = new Schema({
     name: { type: String, required: true },
     meet: { type: Objectid, required: true},
     season: { type: Objectid, required: true},
-    eventType: { type: [Objectid], required: true}
+    eventType: {
+        name: { type: String, required: true},
+        description: { type: String, required: true},
+    },
+    isRelay: {type: Boolean, required: true}
 })
 
 module.exports = mongoose.model("Event", eventSchema);
