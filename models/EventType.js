4f5e4e1a6c752c61e439e97ea1fc9eb92b5a2c3f
Unknown <audeer2019@mirmanmustangs.org>
2017-05-01 14:12:08 -0700

    Update index.js
    I updated index.js to include our new models

--- models/EventType.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const eventTypeSchema = new Schema({
-    name: { type: String, required: true},
-    description: { type: String, required: true},
-})
-
-module.exports = mongoose.model("EventType", eventTypeSchema);
